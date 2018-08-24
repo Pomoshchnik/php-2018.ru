111eb812a802ca16d442f30161fb3648c76c3d6c
Денис <38973885+Pomoshchnik@users.noreply.github.com>
2018-08-24 23:54:57 +0300

    Add files via upload

---
+++ money.php
@@ -0,0 +1,27 @@
+<?
+
+echo "Привет!<br/><br/>";
+
+$handle = fopen("data.txt", "ab");	// Ресурс
+$date = date('(Y-m-d) H:m:s');		// Дата покупки
+$title_buy = "Описание покупки";	// Описание покупки
+$price = "250" . " р.";				// Стоимость покупки
+
+$rec_buy = $date . " || " . $price . " - " . $title_buy;
+fwrite($handle, $rec_buy);
+fclose($handle)
+
+
+
+?>
+
+
+
+
+
+
+
+
+
+
+
